ee1f189abfb9a020123797cdb34b3fae602668d2
Trevor <trevorjbaron@gmail.com>
2014-06-10 02:40:43 -0400

    update to send ui and handle range and button events

--- examples/pc.js
+++ examples/pc.js
@@ -1,12 +1,22 @@
+var fs = require('fs');
 var slab = require("../lib/slab")
+var exec = require('child_process').exec;
 
 var handlers = []
-handlers.push(slab.createHandler("volume", slab.CONTROL_TYPE.RANGE, handleVol))
 
-var handleVol = function *(val){
-	console.log(val)
+var handleVol = function *(e){
+	exec("amixer -D pulse sset Master "+e.val+"%", function (error, stdout, stderr) {});
 }
 
-var server = slab.startServer(handlers, "test", {port: 3000})
+var handleButton = function *(e){
+	exec("shut", function (error, stdout, stderr) {});
+}
+
+handlers.push(slab.createHandler("volume", slab.CONTROL_TYPE.RANGE, handleVol))
+handlers.push(slab.createHandler("outputText", slab.CONTROL_TYPE.BUTTON, handleButton))
+
+
+
+var server = slab.startServer(handlers, fs.readFileSync('ui.html').toString(), {port: 3000})
 
 console.log("Started running on address " + server.ip+":"+server.port);
